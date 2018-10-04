60f4b84a153bdd6511e1b6e436d79a468378f246
Zaid Bin Tariq <zaid@dapperapps.com.au>
2018-10-05 01:18:25 +0500

    temprature parsing and mapping added

---
+++ app/src/main/java/com/framework/openweatherandroidapp/utils/WeatherUtils.kt
@@ -0,0 +1,104 @@
+package com.framework.openweatherandroidapp.utils
+
+import com.framework.openweatherandroidapp.R
+import java.util.*
+
+class WeatherUtils private constructor() {
+
+    enum class TEMP_UNIT {
+        Celsius, Fahrenheit, Kelvin
+    }
+
+    enum class WIND_UNIT {
+        MS, KMH, MPH
+    }
+
+    companion object {
+
+        fun getTempString(kelvinTemp: Double, tempUnit: TEMP_UNIT): String {
+
+            return when (tempUnit) {
+                TEMP_UNIT.Celsius -> {
+                    val celsiusTemp = convertToCelsius(kelvinTemp)
+                    String.format(Locale.ENGLISH, "%.1f °C", celsiusTemp)
+                }
+                TEMP_UNIT.Fahrenheit -> {
+                    val fahrenheitTemp = convertToFahrenheit(kelvinTemp)
+                    String.format(Locale.ENGLISH, "%.1f °F", fahrenheitTemp)
+                }
+                TEMP_UNIT.Kelvin -> String.format(Locale.ENGLISH, "%.1f K", kelvinTemp)
+            }
+        }
+
+        private fun convertToCelsius(kelvinTemp: Double): Double {
+            return kelvinTemp - 273.15
+        }
+
+        private fun convertToFahrenheit(kelvinTemp: Double): Double {
+            return kelvinTemp * 9 / 5.0 - 459.67
+        }
+
+        private fun convertToKmh(msWindSpeed: Double): Double {
+            return msWindSpeed * 3.6
+        }
+
+        private fun convertToMph(msWindSpeed: Double): Double {
+            return msWindSpeed * 2.2369
+        }
+
+//    fun getCoordinatesString(latLng: LatLng): String {
+//        return String.format(Locale.ENGLISH, "(%.2f, %.2f)", latLng.latitude, latLng.longitude)
+//    }
+
+        fun getHumidityString(humidity: Int): String {
+            return String.format(Locale.ENGLISH, "Humidity:  %d %%", humidity)
+        }
+
+        fun getWindString(msWindSpeed: Double, windUnit: WIND_UNIT): String {
+
+            return when (windUnit) {
+                WIND_UNIT.MS -> String.format(Locale.ENGLISH, "Wind:  %.1f m/s", msWindSpeed)
+                WIND_UNIT.KMH -> {
+                    val kmhWindSpeed = convertToKmh(msWindSpeed)
+                    String.format(Locale.ENGLISH, "Wind:  %.1f km/h", kmhWindSpeed)
+                }
+                WIND_UNIT.MPH -> {
+                    val mphWindSpeed = convertToMph(msWindSpeed)
+                    String.format(Locale.ENGLISH, "Wind:  %.1f mph", mphWindSpeed)
+                }
+            }
+        }
+
+        fun getExtremeTempsString(maxTemp: Double, minTemp: Double, tempUnit: TEMP_UNIT): String {
+            val maxTempString = getTempString(maxTemp, tempUnit)
+            val minTempString = getTempString(minTemp, tempUnit)
+            return String.format(Locale.ENGLISH, "Min:  %s  |  Max:  %s", minTempString, maxTempString)
+        }
+
+        fun getWeatherImageId(weatherId: Int): Int {
+            return if (weatherId == 800 || weatherId == 904) {
+                R.drawable.ic_clear
+            } else if (weatherId == 801) {
+                R.drawable.ic_few_clouds
+            } else if (weatherId == 802) {
+                R.drawable.ic_scattered_clouds
+            } else if (weatherId == 803 || weatherId == 804) {
+                R.drawable.ic_broken_clouds
+            } else if (weatherId in 200..299) {
+                R.drawable.ic_thunderstorm
+            } else if (weatherId == 906 || weatherId in 300..399 || weatherId in 520..599) {
+                R.drawable.ic_shower_rain
+            } else if (weatherId == 511 || weatherId == 903 || weatherId in 600..699) {
+                R.drawable.ic_snow
+            } else if (weatherId in 500..509) {
+                R.drawable.ic_rain
+            } else if (weatherId in 701..779) {
+                R.drawable.ic_mist
+            } else if (weatherId in 951..959) {
+                R.drawable.ic_windy
+            } else {
+                R.drawable.ic_hurricane
+            }
+        }
+    }
+}
--- app/src/main/java/com/framework/openweatherandroidapp/view/main/MainActivity.kt
+++ app/src/main/java/com/framework/openweatherandroidapp/view/main/MainActivity.kt
@@ -1,15 +1,24 @@
 package com.framework.openweatherandroidapp.view.main
 
+import android.app.SearchManager
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.widget.SearchView
 import android.util.Log
+import android.view.Menu
+import android.widget.Toast
 import com.framework.openweatherandroidapp.App
 import com.framework.openweatherandroidapp.R
 import com.framework.openweatherandroidapp.model.WeatherModel
+import com.framework.openweatherandroidapp.utils.WeatherUtils
+import com.framework.openweatherandroidapp.utils.isConnectedToInternet
+import com.framework.openweatherandroidapp.utils.showToast
 import com.framework.openweatherandroidapp.view.AppBaseActivity
+import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
 class MainActivity : AppBaseActivity<MainViewModel>(), MainViewModelNavigation {
-
 
     @Inject
     lateinit var mainViewModel: MainViewModel
@@ -22,6 +31,9 @@ class MainActivity : AppBaseActivity<MainViewModel>(), MainViewModelNavigation {
         super.onCreate(savedInstanceState)
 
         mainViewModel.setNavigation(this)
+        handleIntent(intent)
+
+//        setSupportActionBar(toolbar)
     }
 
     override fun getViewModel(): MainViewModel {
@@ -31,15 +43,68 @@ class MainActivity : AppBaseActivity<MainViewModel>(), MainViewModelNavigation {
 
     override fun onResume() {
         super.onResume()
-        mainViewModel.fetchWeather("Lahore")
+        fetchWeather("Lahore")
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        handleIntent(intent)
+    }
+
+    private fun handleIntent(intent: Intent) {
+
+        if (Intent.ACTION_SEARCH == intent.action) {
+            val query = intent.getStringExtra(SearchManager.QUERY)
+            if (query.isNotEmpty()) {
+                fetchWeather(query)
+            }
+        }
+    }
+
+    private fun fetchWeather(query: String) {
+        if (isConnectedToInternet()) {
+            mainViewModel.fetchWeather(query)
+        } else {
+            showToast("No Internet Connection",Toast.LENGTH_LONG)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_options, menu)
+
+        // Associate searchable configuration with the SearchView
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        (menu.findItem(R.id.action_search).actionView as SearchView).apply {
+            setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        }
+
+        return true
     }
 
     override fun onGetData(weatherModel: WeatherModel) {
-        Log.d("Weather",weatherModel.toString())
+        Log.d("Weather", weatherModel.toString())
+
+        val weather = weatherModel.weather[0]
+        val weatherImgId = WeatherUtils.getWeatherImageId(weatherId = weather.id)
+        img_weather.setImageResource(weatherImgId)
+
+        tv_city_name.text = weatherModel.name
+        tv_extreme_temp.text = WeatherUtils.getExtremeTempsString(weatherModel.main.tempMax, weatherModel.main.tempMin, WeatherUtils.TEMP_UNIT.Celsius)
+        tv_cur_temp.text = WeatherUtils.getTempString(weatherModel.main.temp, WeatherUtils.TEMP_UNIT.Celsius)
+        tv_humidity.text = WeatherUtils.getHumidityString(weatherModel.main.humidity)
+        tv_wind_speed.text = WeatherUtils.getWindString(weatherModel.wind.speed, WeatherUtils.WIND_UNIT.KMH)
+        tv_description.text = weather.description
+
     }
 
     override fun onErrorReturn(error: String?) {
-        Log.e("Weather-Error",error)
+
+        if (!isConnectedToInternet()) {
+            showToast("No Internet Connection",Toast.LENGTH_LONG)
+        } else {
+            showToast(error!!,Toast.LENGTH_LONG)
+        }
     }
 
 }
